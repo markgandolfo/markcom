ca2f3d232a4842103db4c77a98d2bfc9efb74b54
Mark Gandolfo <mark@gandolfo.com.au>
2020-08-25 17:40:39 +1000

    new changes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9852d42261c0a9175ad86df506bb191.js"
+  "/precache-manifest.7bdcdd76b16fde1b738c458e4a00294b.js"
 );
 
 self.addEventListener('message', (event) => {
